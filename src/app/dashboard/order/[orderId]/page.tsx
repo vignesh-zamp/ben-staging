ee74b278d789ffa77595a013b1a2d20740a96989
Vignesh Narayanan <vignesh@zamp.ai>
2025-11-07 19:57:01 +0000

    Can you add a new row here as well, ONLY FOR THIS STOCK #10538, with Acq

--- src/app/dashboard/order/[orderId]/page.tsx
+++ src/app/dashboard/order/[orderId]/page.tsx
@@ -330,7 +330,8 @@ const initialOrderData: { [key: string]: any } = {
                 statusColor: 'text-green-500',
                 tasks: [
                     { team: "Acquisition", assignedTo: "Shyda Gonzales", task: "Drivers License Verification", badge: "EXT", badgeColor: "bg-blue-500", state: "Pending Verification", stateDate: "", documents: "1/1 file(s)", dueDate: "-", documentUrl: "/image4.png" },
-                    { team: "Acquisition", assignedTo: "Khushi Patel", task: "Drivers License Verification (second approval)", badge: "INT", badgeColor: "bg-red-500", state: "Complete", stateDate: "", documents: "-", dueDate: "-", }
+                    { team: "Acquisition", assignedTo: "Khushi Patel", task: "Drivers License Verification (second approval)", badge: "INT", badgeColor: "bg-red-500", state: "Complete", stateDate: "", documents: "-", dueDate: "-", },
+                    { team: "Acquisition", assignedTo: "Khushi Patel", task: "Ontario Photo ID", badge: "EXT", badgeColor: "bg-blue-500", state: "Pending Verification", stateDate: "", documents: "1/1 file(s)", dueDate: "-", documentUrl: doc1 }
                 ]
             },
             {
